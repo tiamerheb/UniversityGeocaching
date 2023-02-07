75153ef312abbe5cf240bc123debbefaeb2f3b12
Tia Merheb <tmerheb@sandiego.edu>
2023-02-07 14:48:50 -0800

    Initial Commit

---
+++ UniversityGeocaching/UniversityGeocachingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityGeocachingApp.swift
+//  UniversityGeocaching
+//
+//  Created by Tia Merheb on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct UniversityGeocachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
