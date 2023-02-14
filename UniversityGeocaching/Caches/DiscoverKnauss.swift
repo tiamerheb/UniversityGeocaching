be9806566efcc463fb1c746bb2ce59238411819d
Tia Merheb <tmerheb@sandiego.edu>
2023-02-14 15:39:45 -0800

    Made Cache buttons and files which correspond to the buttons

---
+++ UniversityGeocaching/Caches/DiscoverKnauss.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverKnauss.swift
+//  UniversityGeocaching
+//
+//  Created by Tia Merheb on 2/14/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct DiscoverKnauss : View{
+    var body : some View{
+        Text("Discover Knauss")
+            .font(.largeTitle)
+        .navigationBarBackButtonHidden()
+    }
+}
--- UniversityGeocaching/Navigation/NearbyQuestView.swift
+++ UniversityGeocaching/Navigation/NearbyQuestView.swift
@@ -10,7 +10,50 @@ import SwiftUI
 
 struct NearbyQuestView : View{
     var body : some View{
-        Text("Nearby Quest View")
-            .font(.largeTitle)
+        ScrollView(.vertical){
+            NavigationView{
+                VStack{
+                    VStack(alignment: .leading) {
+                        Text("Nearby Quests")
+                            .font(.headline)
+                    }
+                    HStack{
+                        NavigationLink(destination: DiscoverKnauss()){
+                            HStack{
+                                VStack{
+                                    Text("Discover Knauss")
+                                        .font(.headline)
+                                    Image("KNAUSS")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                    Text("Explore USD's new business building")
+                                }
+                            }
+                            .frame(width: 150, height: 250)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(15)
+                        }
+                        NavigationLink(destination: CampusCafes()){
+                            HStack{
+                                VStack{
+                                    Text("Campus Cafes")
+                                        .font(.headline)
+                                    //Image("KNAUSS")
+                                    //.resizable()
+                                    //.aspectRatio(contentMode: .fit)
+                                }
+                            }
+                            .frame(width: 150, height: 250)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(15)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
