d084f6e73fe1d24d23452f753ede7be002ef6a95
Ewowodadon <83219152+ewhondens@users.noreply.github.com>
2023-05-09 12:27:04 -0700

    nearby quests done

--- UniversityGeocaching/Navigation/NearbyQuestView.swift
+++ UniversityGeocaching/Navigation/NearbyQuestView.swift
@@ -1,52 +1,27 @@
 import SwiftUI
 import Combine
-import MapKit
-import CoreLocation
 import Foundation
+import CoreLocation
+import MapKit
 
-// QuestWrapper is a class that wraps a CreatedQuest and adds an observable "completed" property.
-class QuestWrapper: ObservableObject, Identifiable {
-    let quest: CreatedQuest
-    @Published var completed: Bool
-
-    init(quest: CreatedQuest) {
-        self.quest = quest
-        self.completed = quest.completed
-    }
-}
-
-// NearbyQuestView displays a list of nearby quests.
 struct NearbyQuestView: View {
-    @Environment(\.presentationMode) var presentationMode
-    @State private var nearbyQuests: [QuestWrapper] = []
+    @State private var nearbyQuests: [CreatedQuest] = []
     @StateObject private var cancellables = Cancellables()
-    @StateObject private var viewModel = NearbyQuestViewModel()
+    @StateObject private var locationManager = LocationManager()
 
-    // The body of the NearbyQuestView.
     var body: some View {
         NavigationView {
-            List {
-                // Display a list of nearby quests.
-                ForEach(nearbyQuests) { questWrapper in
-                    NavigationLink(destination: QuestDetailView(quest: questWrapper.quest)) {
-                        HStack {
-                            QuestCardView(quest: questWrapper.quest)
-                            CheckBox(questWrapper: questWrapper)
-                        }
-                    }
+            List(nearbyQuests, id: \.id) { quest in
+                NavigationLink(destination: QuestMapView(quest: quest)) {
+                    Text("Cache Name: \(quest.cachename)")
                 }
             }
             .navigationBarTitle(Text("Nearby Quests"))
-            .navigationBarItems(trailing: Button(action: {
-                self.presentationMode.wrappedValue.dismiss()
-            }, label: {
-                Text("Close")
-            }))
+            .onAppear(perform: loadData)
         }
-        .onAppear(perform: loadData)
     }
 
-    // Fetches quests from the server and updates the nearbyQuests property.
+
     func loadData() {
         fetchQuests()
             .sink(receiveCompletion: { completion in
@@ -59,24 +34,18 @@ struct NearbyQuestView: View {
             }, receiveValue: { quests in
                 print("Received quests: \(quests)")
                 DispatchQueue.main.async {
-                    viewModel.filterAndSortQuests(quests)
-                    nearbyQuests = viewModel.nearbyQuests
+                    self.nearbyQuests = self.sortQuestsByDistance(quests)
                 }
             })
             .store(in: &cancellables.storage)
     }
 
-
-    // Fetches quests from the server using URLSession and Combine.
     func fetchQuests() -> AnyPublisher<[CreatedQuest], Error> {
         let url = URL(string: "http://universitygeocaching.azurewebsites.net/api/location")!
 
-        // Create a URLRequest with custom headers
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         request.setValue("*/*", forHTTPHeaderField: "accept")
-
-        // Set the authorization token
         request.setValue("starterKey420", forHTTPHeaderField: "X-Auth")
 
         return URLSession.shared.dataTaskPublisher(for: request)
@@ -96,174 +65,88 @@ struct NearbyQuestView: View {
             .eraseToAnyPublisher()
     }
 
+    func sortQuestsByDistance(_ quests: [CreatedQuest]) -> [CreatedQuest] {
+        guard let userLocation = locationManager.lastKnownLocation else {
+            return quests
+        }
 
+        return quests.sorted { quest1, quest2 in
+            let quest1Location = CLLocation(latitude: quest1.latitude, longitude: quest1.longitude)
+            let quest2Location = CLLocation(latitude: quest2.latitude, longitude: quest2.longitude)
+            return userLocation.distance(from: quest1Location) < userLocation.distance(from: quest2Location)
+        }
+    }
 }
 
-// Cancellables is a class that holds a set of AnyCancellables.
 class Cancellables: ObservableObject {
     var storage = Set<AnyCancellable>()
 }
 
-// CreatedQuest is a struct representing a created quest.
 struct CreatedQuest: Identifiable, Codable {
     let id: Int
-    var uuid: UUID {
-        return UUID(uuidString: "\(id)")!
-    }
     let cachename: String
-    let difficulty: Int
-    let hints: String
     let latitude: Double
     let longitude: Double
-    let radius: Int
-    let timestamp: String
-    let trivia: String
-    let users: [String] // Assuming users is an array of strings
-    let verificationString: String
-    var completed: Bool = false
-
-    enum CodingKeys: String, CodingKey {
-        case id, cachename, difficulty, hints, latitude, longitude, radius, timestamp, trivia, users, verificationString
-    }
 }
 
-
-
-
-// QuestCardView displays a single quest card.
-struct QuestCardView: View {
-    let quest: CreatedQuest
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            Text(quest.cachename)
-                .font(.headline)
-
-            // Display the quest's hints, difficulty, and other information
-            Text("Hints: \(quest.hints)")
-                .font(.footnote)
-                .foregroundColor(.secondary)
-
-            Text("Difficulty: \(quest.difficulty)")
-                .font(.footnote)
-                .foregroundColor(.secondary)
-
-            Text("Trivia: \(quest.trivia)")
-                .font(.footnote)
-                .foregroundColor(.secondary)
-
-            Text("Verification String: \(quest.verificationString)")
-                .font(.footnote)
-                .foregroundColor(.secondary)
-        }
-        .padding()
-        .background(Color(.systemGroupedBackground))
-        .cornerRadius(10)
-        .shadow(radius: 5)
-    }
-}
-
-
-// CheckBox is a custom view that displays a checkmark when the quest is completed.
-struct CheckBox: View {
-    @ObservedObject var questWrapper: QuestWrapper
-
-    var body: some View {
-        Button(action: {
-            questWrapper.completed.toggle()
-        }) {
-            Image(systemName: questWrapper.completed ? "checkmark.square" : "square")
-                .resizable()
-                .frame(width: 24, height: 24)
-                .foregroundColor(questWrapper.completed ? .blue : .gray)
-        }
-    }
-}
-
-// QuestDetailView displays the details of a single quest.
-struct QuestDetailView: View {
-    let quest: CreatedQuest
-
-    var body: some View {
-        VStack {
-            Text(quest.cachename) // Use 'cachename' instead of 'title'
-                .font(.largeTitle)
-                .padding()
-            Text(quest.trivia) // Use 'trivia' instead of 'description'
-                .font(.body)
-                .padding()
-            // Display the quest's location on a map
-            MapView(coordinate: CLLocationCoordinate2D(latitude: quest.latitude, longitude: quest.longitude))
-                .frame(height: 300)
-        }
-    }
-}
-
-
-// MapView is a UIViewRepresentable that displays a map with a single annotation.
-struct MapView: UIViewRepresentable {
-    let coordinate: CLLocationCoordinate2D
-
-    func makeUIView(context: Context) -> MKMapView {
-        let mapView = MKMapView()
-        mapView.delegate = context.coordinator
-        return mapView
-    }
-
-    func updateUIView(_ uiView: MKMapView, context: Context) {
-        let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
-        uiView.setRegion(region, animated: true)
-
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = coordinate
-        uiView.addAnnotation(annotation)
-    }
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    class Coordinator: NSObject, MKMapViewDelegate {
-        var parent: MapView
-
-        init(_ parent: MapView) {
-            self.parent = parent
-        }
-    }
-}
-
-// NearbyQuestViewModel is a class that handles filtering and sorting quests based on location.
-class NearbyQuestViewModel: ObservableObject {
-    @Published var nearbyQuests: [QuestWrapper] = []
-
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private var locationManager = CLLocationManager()
-    private var currentLocation: CLLocation? {
-        locationManager.location
-    }
+    @Published var lastKnownLocation: CLLocation?
 
-    init() {
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
-    // Filters and sorts quests based on the user's current location.
-    func filterAndSortQuests(_ quests: [CreatedQuest]) {
-        guard let currentLocation = currentLocation else {
-            return
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            lastKnownLocation = location
         }
+    }
 
-        let wrappedQuests = quests.map { QuestWrapper(quest: $0) }
-        let filteredQuests = wrappedQuests.filter { !$0.completed }
-        nearbyQuests = filteredQuests.sorted {
-            let quest1Location = CLLocation(latitude: $0.quest.latitude, longitude: $0.quest.longitude)
-            let quest2Location = CLLocation(latitude: $1.quest.latitude, longitude: $1.quest.longitude)
-            return currentLocation.distance(from: quest1Location) < currentLocation.distance(from: quest2Location)
-        }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager failed with error: \(error)")
     }
 }
+
+
+struct QuestMapView: View {
+    var quest: CreatedQuest
+    @State private var region: MKCoordinateRegion
+    
+    init(quest: CreatedQuest) {
+        self.quest = quest
+        self._region = State(initialValue: MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: quest.latitude, longitude: quest.longitude), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)))
+    }
+    
+    var body: some View {
+        Map(coordinateRegion: $region, annotationItems: [quest]) { quest in
+            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: quest.latitude, longitude: quest.longitude)) {
+                Image(systemName: "mappin.circle")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+            }
+        }
+        .navigationBarTitle(Text(quest.cachename), displayMode: .inline)
+    }
+}
+
+struct QuestMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestMapView(quest: CreatedQuest(id: 0, cachename: "Sample Cache", latitude: 37.7749, longitude: -122.4194))
+    }
+}
+
+
 
 struct NearbyQuestView_Previews: PreviewProvider {
     static var previews: some View {
         NearbyQuestView()
     }
 }
+
+
 
